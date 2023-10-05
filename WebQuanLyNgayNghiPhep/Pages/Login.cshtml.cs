d6eb1e6096471b761cc304a1d7c9fd98a5cdc54b
Kairyu14 <101346991+Kairyu14@users.noreply.github.com>
2023-10-05 15:26:35 +0700

    add code

---
+++ WebQuanLyNgayNghiPhep/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebQuanLyNgayNghiPhep.Pages
+{
+    public class _LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebQuanLyNgayNghiPhep/Pages/NhanVien.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebQuanLyNgayNghiPhep.Pages
+{
+    public class NhanVienModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
